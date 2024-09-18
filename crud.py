9bb36501fa43ee3342ee902ac33b52cae3c3f4ef
punitk91 <pkulkarni290@gmail.com>
2024-09-18 21:43:24 +0530

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+def create_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(name=item.name, description=item.description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_item(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
